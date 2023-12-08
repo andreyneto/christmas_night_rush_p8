d884e0c321e1d3ed1bb99f2a8b3b2b0a79064409
Andrey Neto <contato@andreyneto.com.br>
2023-12-08 07:05:01 -0300

    Create particles.lua

---
+++ entities/particles.lua
@@ -0,0 +1,35 @@
+-- particle
+particle=entity:extend({
+	frame=0,
+	frames=10,
+	rad=1,
+	clr=5,
+	sx=0,
+	sy=0,
+	
+	update=function(_ENV)
+		x+=sx
+		y+=sy
+		frame+=1
+		
+		if frame>=frames then
+			destroy(_ENV)
+		end
+	end,
+
+	draw=function(_ENV)
+		local r=(1-frame/frames)*rad
+		circfill(x,y,r,clr)
+	end
+})
+
+dust=particle:extend({
+	rad=2,
+	sy=.25,
+	clr=7
+})
+
+spark=particle:extend({
+	clr=10,
+	sy=-.5,
+})
