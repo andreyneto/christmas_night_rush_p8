df546b4942651448968ced55ed8b320de6611b6f
Andrey Neto <contato@andreyneto.com.br>
2023-12-08 07:05:17 -0300

    Update game_scene.lua

--- scenes/game_scene.lua
+++ scenes/game_scene.lua
@@ -5,16 +5,16 @@ game_scene=scene:extend({
 
 	update=function(_ENV)
 		entity:each("update")
-		if btnp(❎) then
-			scene:load(title_scene)
-		end
 	end,
 
 	draw=function(_ENV)
 		add(entity.pool,del(entity.pool,player))
 		map(0,0)
-		prints("score: "..pad(score.."00",7),4,4,7)
-		prints("time: "..pad(ceil(timer/30),2),93,4,7)
+
+		prints("x: "..player.x,4,4,7)
+		prints("y: "..player.y,4,12,7)
+		-- prints("score: "..pad(score.."00",7),4,4,7)
+		-- prints("time: "..pad(ceil(timer/30),2),93,4,7)
 		entity:each("draw")
 	end,
 })
