0dcf595951667dd2e0c4a773db2bfe1405142dcc
Andrey Neto <contato@andreyneto.com.br>
2023-12-10 21:55:11 -0300

    Update title_scene.lua

--- scenes/title_scene.lua
+++ scenes/title_scene.lua
@@ -1,34 +1,58 @@
 title_scene=scene:extend({
+
+	options={
+		{
+			label=strings.start_game,
+			call=function()
+				scene:load(game_scene)
+			end
+		},
+		{
+			label=strings.achievements,
+			call=noop
+		},
+		{
+			label=strings.leaderboard,
+			call=noop
+		},
+		{
+			label=strings.language,
+			call=function()
+				if(language==1) then language = 2
+				else language = 1 end
+				dset(0, language)
+			end
+		},
+	},
+	selected=1,
+
 	init=function(_ENV)
 	end,
 
 	update=function(_ENV)
 		entity:each("update")
-	
+		if btnp(⬇️) then
+			repeat selected += 1 until options[selected].call != noop
+		end
+		if btnp(⬆️) then
+			repeat selected -= 1 until options[selected].call != noop
+		end
+		selected=mid(1,selected,#options)
 		if btnp(❎) then
-			scene:load(game_scene)
+			options[selected]:call()
 		end
 	end,
 
-	draw=function()
-		-- map(0,0)
-		-- entity:each("draw")
-		for j=0, 2 do
-			for i=0, 12 do
-				spr(192+i+(j*16), 16+(i*8), 12+32+(j*8))
+	draw=function(_ENV)
+		spr(128,8,56,14,2)
+		spr(192,16,32,12,3)
+		local p = 64+((64-(#options*8))/2)-8
+		for i=1, #options do
+			local is_selected = i==selected
+			printc((is_selected and "> " or "")..options[i].label[language]..(is_selected and " <" or ""), p+(8*i), is_selected and 10 or (options[i].call == noop) and 6 or 7, is_selected)
+			if(i == #options) then
 			end
 		end
-		for j=0, 1 do
-			for i=0, 14 do
-				spr(128+i+(j*16), 8+(i*8), 12+56+(j*8))
-			end
-		end
-		
-		-- instructions
-		-- printc("collect all the hearts",60,6)
-		-- printc("before time runs out!",68,6)
-		
-		-- prompt
-		printc("❎ start game",96,7)
+		spr(languages[language][2], 4, 4, 2, 1)
 	end,
 })
