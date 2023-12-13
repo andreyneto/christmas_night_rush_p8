9b387c37dfc7b2d5bef337d35768d3f92f09c3f8
Andrey Neto <contato@andreyneto.com.br>
2023-12-13 16:25:35 -0300

    Update game_scene.lua

--- scenes/game_scene.lua
+++ scenes/game_scene.lua
@@ -1,4 +1,6 @@
 game_scene=scene:extend({
+	steps=0,
+	score=0,
 	init=function(_ENV)
 		player=santa()
 		chunk()
@@ -9,13 +11,29 @@ game_scene=scene:extend({
 		chunk:each("update")
 		entity:each("update")
 		entity:each("animate")
+		steps+=speed
+		if(steps>=128) then
+			score +=1
+			steps=0
+		end
 	end,
 
 	draw=function(_ENV)
 		add(entity.pool,del(entity.pool,player))
 		chunk:each("draw")
-		prints("speed: "..speed,4,4,7)
-		-- prints("score: "..pad(score.."00",7),4,4,7)
+		local gui = {
+			{strings.score,fill_number(flr(score)).."00",0,204},
+			{strings.coins,fill_number(flr(score)).."00",42,32},
+		}
+		foreach(gui, function (i)
+			local label = i[1]
+			local value = i[2]
+			local posit = i[3] + 4
+			local sprit = i[4]
+			spr(sprit, posit, 5)
+			prints(value,posit+10,4,7)
+			prints(label[language],posit+10,9,6)
+		end)
 		-- prints("time: "..pad(ceil(timer/30),2),93,4,7)
 		entity:each("draw")
 	end,
